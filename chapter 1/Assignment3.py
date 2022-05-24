5b9b2a18ff67078fba8b6beb02a162e0bdf6b84a
KeerthanaBasavaraj <69410571+KeerthanaBasavaraj@users.noreply.github.com>
2022-05-24 22:55:40 +0530

    Update Assignment3.py

--- chapter 1/Assignment3.py
+++ chapter 1/Assignment3.py
@@ -2,7 +2,7 @@
 
 def armstrong_number(user_number):
     # Initializing Sum and Number of Digits
-    n_power_digits_sum = 0
+    digits_sum = 0
     number_of_digits = 0
 
     # Calculating Number of individual digits
@@ -15,9 +15,9 @@ def armstrong_number(user_number):
     user_number_copy = user_number
     for i in range(1, number_of_digits + 1):
         digit = user_number_copy % 10
-        n_power_digits_sum = n_power_digits_sum + (digit ** number_of_digits)
+        digits_sum = digits_sum + (digit ** number_of_digits)
         user_number_copy //= 10
-    return n_power_digits_sum
+    return digits_sum
 
 # End of Function
 
